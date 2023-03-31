7884bff361a456e1c1184efdb2b6b421e51d9a59
Luigi160397 <luigi.difraia.97@gmail.com>
2023-03-31 13:07:07 +0200

    situazione dinamica

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,58 +1,65 @@
 import { Component } from "react";
 import MyHeader from "./MyHeader";
 import Movie from "./Movie";
-import image from "../assets/media/media1.jpg";
 
 class Home extends Component {
+  state = {
+    HarryPotter: [],
+    StarWars: [],
+    LordOfTheRing: []
+  };
+
+  request = async (endpoint, stato) => {
+    try {
+      const response = await fetch(endpoint);
+      const data = await response.json();
+      this.setState({ [stato]: data.Search });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  componentDidMount() {
+    this.request("http://www.omdbapi.com/?apikey=5f735187&s=harry%20potter", "HarryPotter");
+    this.request("https://www.omdbapi.com/?apikey=5f735187&s=star%20wars", "StarWars");
+    this.request("https://www.omdbapi.com/?apikey=5f735187&s=lord%20of%20the%20rings", "LordOfTheRing");
+  }
   render() {
     return (
       <div className="container-fluid">
         <MyHeader />
         <div className="movie-gallery mx-md-5 mb-5 mt-4">
-          <h5 className="text-light mt-2 mb-2">Trending Now</h5>
+          <h5 className="text-light mt-2 mb-2">Harry Potter</h5>
           <div id="trending-now">
             <div className="movie-row">
               <div className="row g-1 flex-nowrap movie-list py-2">
-                <Movie src={image} alt="movie" />
-                <Movie src={image} alt="movie" />
-                <Movie src={image} alt="movie" />
-                <Movie src={image} alt="movie" />
-                <Movie src={image} alt="movie" />
-                <Movie src={image} alt="movie" />
-                <Movie src={image} alt="movie" />
-                <Movie src={image} alt="movie" />
-                <Movie src={image} alt="movie" />
-                <Movie src={image} alt="movie" />
+                {this.state.HarryPotter.map(movie => (
+                  <Movie key={movie.imdbID} src={movie.Poster} alt={movie.Title} />
+                ))}
               </div>
             </div>
           </div>
         </div>
         <div className="movie-gallery mx-md-5 mb-5 mt-4">
-          <h5 className="text-light mt-2 mb-2">Watch It Again</h5>
+          <h5 className="text-light mt-2 mb-2">Star wars</h5>
           <div id="trending-now">
             <div className="movie-row">
               <div className="row g-1 flex-nowrap movie-list py-2">
-                <Movie src={image} alt="movie" />
-                <Movie src={image} alt="movie" />
-                <Movie src={image} alt="movie" />
-                <Movie src={image} alt="movie" />
-                <Movie src={image} alt="movie" />
-                <Movie src={image} alt="movie" />
+                {this.state.StarWars.map(movie => (
+                  <Movie key={movie.imdbID} src={movie.Poster} alt={movie.Title} />
+                ))}
               </div>
             </div>
           </div>
         </div>
         <div className="movie-gallery mx-md-5 mb-5 mt-4">
-          <h5 className="text-light mt-2 mb-2">New Releases</h5>
+          <h5 className="text-light mt-2 mb-2">Lord of the rings</h5>
           <div id="trending-now">
             <div className="movie-row">
               <div className="row g-1 flex-nowrap movie-list py-2">
-                <Movie src={image} alt="movie" />
-                <Movie src={image} alt="movie" />
-                <Movie src={image} alt="movie" />
-                <Movie src={image} alt="movie" />
-                <Movie src={image} alt="movie" />
-                <Movie src={image} alt="movie" />
+                {this.state.LordOfTheRing.map(movie => (
+                  <Movie key={movie.imdbID} src={movie.Poster} alt={movie.Title} />
+                ))}
               </div>
             </div>
           </div>
